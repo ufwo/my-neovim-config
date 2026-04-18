c1d9a6276ee03c617d908f288ae9a4baa03bdcd6
lolcatswag <ufw000@gmail.com>
2026-04-19 02:53:06 +0300

    fuck vim global

--- init.lua
+++ init.lua
@@ -16,42 +16,11 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/conform.nvim" },
 })
 
+vim.cmd.colorscheme("kanagawa-dragon")
+
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "stylua", "rust_analyzer" },
-})
-
-vim.lsp.config("lua_ls", {
-	on_init = function(client)
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-			if
-				path ~= vim.fn.stdpath("config")
-				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-			then
-				return
-			end
-		end
-
-		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-			runtime = {
-				version = "LuaJIT",
-				path = {
-					"lua/?.lua",
-					"lua/?/init.lua",
-				},
-			},
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME,
-				},
-			},
-		})
-	end,
-	settings = {
-		Lua = {},
-	},
 })
 
 require("blink.cmp").setup({
@@ -68,7 +37,8 @@ require("blink.cmp").setup({
 	},
 })
 
-require("conform").setup({
+local conform = require("conform")
+conform.setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		rust = { "rustfmt" },
@@ -78,9 +48,6 @@ require("conform").setup({
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-		--vim.lsp.buf.format({ async = false })
+		conform.format({ bufnr = args.buf })
 	end,
 })
-
-vim.cmd.colorscheme("kanagawa-dragon")
