577f91ae27c287fe52c737e6822e0cdc5b8d1c75
lolcatswag <ufw000@gmail.com>
2026-04-19 03:01:37 +0300

    trying to organize my config...

--- init.lua
+++ init.lua
@@ -4,37 +4,32 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
+---------------------------------------------------------------------
+-- PLUGINS ----------------------------------------------------------
+---------------------------------------------------------------------
+
 vim.pack.add({
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/rebelot/kanagawa.nvim" },
-	{
-		src = "https://github.com/saghen/blink.cmp",
-		version = "v1",
-	},
-	{ src = "https://github.com/stevearc/conform.nvim" },
+	"https://github.com/rebelot/kanagawa.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/mason-org/mason-lspconfig.nvim",
+	"https://github.com/stevearc/conform.nvim",
+	{ src = "https://github.com/saghen/blink.cmp", version = "v1" },
 })
 
+---------------------------------------------------------------------
+-- COLORSCHEME ------------------------------------------------------
+---------------------------------------------------------------------
+
 vim.cmd.colorscheme("kanagawa-dragon")
+
+---------------------------------------------------------------------
+-- LSP'S, FORMATTERS, LINTERS ---------------------------------------
+---------------------------------------------------------------------
 
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "stylua", "rust_analyzer" },
-})
-
-require("blink.cmp").setup({
-	keymap = { preset = "enter" },
-	completion = {
-		documentation = {
-			auto_show = true,
-			auto_show_delay_ms = 200,
-		},
-	},
-	signature = { enabled = true },
-	fuzzy = {
-		implementation = "lua",
-	},
 })
 
 local conform = require("conform")
@@ -50,4 +45,22 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function(args)
 		conform.format({ bufnr = args.buf })
 	end,
+})
+
+---------------------------------------------------------------------
+-- AUTOCOMPLETE -----------------------------------------------------
+---------------------------------------------------------------------
+
+require("blink.cmp").setup({
+	keymap = { preset = "enter" },
+	completion = {
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 200,
+		},
+	},
+	signature = { enabled = true },
+	fuzzy = {
+		implementation = "lua",
+	},
 })
