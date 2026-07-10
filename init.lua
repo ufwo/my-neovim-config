029f4c33cfdeebac43a6e3bbd7380e19c7d96bc5
lolcatswag <ufw000@gmail.com>
2026-07-10 17:27:31 +0300

    Meow

--- init.lua
+++ init.lua
@@ -1,4 +1,18 @@
 ---------------------------------------------------------------------
+-- PLUGINS ----------------------------------------------------------
+---------------------------------------------------------------------
+
+vim.pack.add({
+	"https://github.com/metalelf0/kintsugi-nvim",
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/mason-org/mason-lspconfig.nvim",
+	"https://github.com/nvim-telescope/telescope.nvim",
+	"https://github.com/stevearc/conform.nvim",
+})
+
+---------------------------------------------------------------------
 -- GENERAL ----------------------------------------------------------
 ---------------------------------------------------------------------
 
@@ -9,25 +23,9 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
----------------------------------------------------------------------
--- PLUGINS ----------------------------------------------------------
----------------------------------------------------------------------
+vim.diagnostic.config({ virtual_text = true, update_in_insert = true })
 
-vim.pack.add({
-	"https://github.com/rebelot/kanagawa.nvim",
-	"https://github.com/metalelf0/black-metal-theme-neovim",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/mason-org/mason.nvim",
-	"https://github.com/mason-org/mason-lspconfig.nvim",
-	"https://github.com/stevearc/conform.nvim",
-	{ src = "https://github.com/saghen/blink.cmp", version = "v1" },
-})
-
----------------------------------------------------------------------
--- COLORSCHEME ------------------------------------------------------
----------------------------------------------------------------------
-
-vim.cmd.colorscheme("Taake")
+vim.cmd.colorscheme("kintsugi-flared")
 
 ---------------------------------------------------------------------
 -- LSP'S, FORMATTERS, LINTERS ---------------------------------------
@@ -35,7 +33,22 @@ vim.cmd.colorscheme("Taake")
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "stylua", "rust_analyzer" },
+	ensure_installed = {
+		-- lsp
+
+		"lua_ls",
+		"vtsls",
+		"rust_analyzer",
+
+		-- formatter
+
+		"stylua",
+		"oxfmt",
+
+		-- linter
+
+		"oxlint",
+	},
 })
 
 local conform = require("conform")
@@ -43,6 +56,10 @@ conform.setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		rust = { "rustfmt" },
+		javascript = { "oxfmt" },
+		typescript = { "oxfmt" },
+		json = { "oxfmt" },
+		jsonc = { "oxfmt" },
 	},
 })
 
@@ -57,14 +74,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- AUTOCOMPLETE -----------------------------------------------------
 ---------------------------------------------------------------------
 
-require("blink.cmp").setup({
-	keymap = { preset = "enter" },
-	completion = {
-		documentation = {
-			auto_show = true,
-			auto_show_delay_ms = 200,
-		},
-	},
-	signature = { enabled = true },
-	fuzzy = { implementation = "lua" },
-})
+vim.o.complete = ".,o"
+vim.o.completeopt = "fuzzy,menuone,noselect,popup"
+vim.o.pumheight = 7
+vim.o.autocomplete = true
+
+---------------------------------------------------------------------
+-- TELESCOPE --------------------------------------------------------
+---------------------------------------------------------------------
+
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope.find_files)
